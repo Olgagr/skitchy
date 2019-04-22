782575434a44527e8afdab0731b7ce1db7507863
Olga Grabek <olga.grabek@gmail.com>
2019-04-22 14:27:03 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+      </div>
+    );
+  }
+}
