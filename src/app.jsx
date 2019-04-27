65666fbc6508d2998f01ad03424b68a117526d7a
Olga Grabek <olga.grabek@gmail.com>
2019-04-27 17:13:22 +0200

    Add fabric.js and allow to draw cropping rectangle on the canvas

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { ipcRenderer } from 'electron';
 import React from 'react';
+import { fabric } from 'fabric';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,23 +8,102 @@ export default class App extends React.Component {
     this.state = {
       screenshotPath: null,
     };
+    this.canvas = null;
+    this.inCropMode = false;
+    this.croppingRect = null;
+    this.loadImagePreview = this.loadImagePreview.bind(this);
+    this.crop = this.crop.bind(this);
   }
 
   componentDidMount() {
-    ipcRenderer.on('load-screenshot-preview', (event, imagePath) => {
-      this.setState({ screenshotPath: imagePath });
+    this.canvas = new fabric.Canvas('canvas');
+    this.canvas.setDimensions({ width: 1080, height: 600 });
+    ipcRenderer.on('load-screenshot-preview', this.loadImagePreview);
+  }
+
+  loadImagePreview(event, imagePath) {
+    const canvasCenter = this.canvas.getCenter();
+    fabric.Image.fromURL(imagePath, (i) => {
+      this.canvas.setBackgroundImage(i, this.canvas.renderAll.bind(this.canvas), {
+        originX: 'center',
+        originY: 'center',
+        top: canvasCenter.top,
+        left: canvasCenter.left,
+      });
     });
   }
 
-  screenshotImage() {
-    return <img src={this.state.screenshotPath} alt="aaa" />;
+  crop() {
+    this.inCropMode = !this.inCropMode;
+    if (this.inCropMode && !this.croppingRect) {
+      let isDown;
+      let origX;
+      let origY;
+
+      const onMouseDownHandler = (o) => {
+        isDown = true;
+        let pointer = this.canvas.getPointer(o.e);
+        origX = pointer.x;
+        origY = pointer.y;
+        pointer = this.canvas.getPointer(o.e);
+        this.croppingRect = new fabric.Rect({
+          left: origX,
+          top: origY,
+          originX: 'left',
+          originY: 'top',
+          width: pointer.x - origX,
+          height: pointer.y - origY,
+          angle: 0,
+          fill: 'rgba(255,0,0,0.5)',
+          transparentCorners: false,
+        });
+        this.canvas.add(this.croppingRect);
+      };
+
+      const onMouseMoveHandler = (o) => {
+        if (!isDown) return;
+        const pointer = this.canvas.getPointer(o.e);
+
+        if (origX > pointer.x) {
+          this.croppingRect.set({ left: Math.abs(pointer.x) });
+        }
+        if (origY > pointer.y) {
+          this.croppingRect.set({ top: Math.abs(pointer.y) });
+        }
+
+        this.croppingRect.set({ width: Math.abs(origX - pointer.x) });
+        this.croppingRect.set({ height: Math.abs(origY - pointer.y) });
+
+        this.canvas.renderAll();
+      };
+
+      this.canvas.on('mouse:down', onMouseDownHandler);
+
+      this.canvas.on('mouse:move', onMouseMoveHandler);
+
+      this.canvas.on('mouse:up', () => {
+        isDown = false;
+        this.canvas.off({ 'mouse:down': onMouseDownHandler, 'mouse:move': onMouseMoveHandler });
+      });
+    }
   }
 
   render() {
     return (
-      <div>
-        <h2>To take a screenshot, use CTRL+ALT+L</h2>
-        {this.state.screenshotPath && this.screenshotImage()}
+      <div className="content">
+        <aside>
+          <nav>
+            <ul>
+              <li>
+                <button onClick={this.crop}>Crop</button>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main>
+          {/* <h2>To take a screenshot, use CTRL+ALT+L</h2> */}
+          <canvas id="canvas" width="100%" height="100%" />
+        </main>
       </div>
     );
   }
