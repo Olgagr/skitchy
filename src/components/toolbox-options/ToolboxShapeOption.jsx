2da95d51fc52a0335916ce73141c0093c9a91711
Olga Grabek <olga.grabek@gmail.com>
2019-05-21 17:45:49 +0200

    Allow to draw rectangle

--- src/components/toolbox-options/ToolboxShapeOption.jsx
+++ src/components/toolbox-options/ToolboxShapeOption.jsx
@@ -4,12 +4,34 @@ import { TOOLBOX_OPTION } from '../../constants';
 
 const SHAPE_TYPES = {
   ARROW: 'arrow',
+  RECTANGLE: 'rectangle',
 };
 
 export default class ToolboxShapeOption extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submenuOpened: false,
+    };
+    this.toggleSubmenu = this.toggleSubmenu.bind(this);
+  }
+
   addShape(shapeType) {
     const canvasCenter = this.props.canvas.getCenter();
     switch (shapeType) {
+      case SHAPE_TYPES.RECTANGLE: {
+        const rect = new fabric.Rect({
+          fill: this.props.activeColor,
+          originX: 'center',
+          originY: 'center',
+          top: canvasCenter.top,
+          left: canvasCenter.left,
+          width: 200,
+          height: 100,
+        });
+        this.props.canvas.add(rect);
+        break;
+      }
       case SHAPE_TYPES.ARROW:
       default:
         fabric.loadSVGFromString(
@@ -29,6 +51,11 @@ export default class ToolboxShapeOption extends Component {
           }
         );
     }
+    this.props.optionClickHandler(TOOLBOX_OPTION.ADD_SHAPE);
+  }
+
+  toggleSubmenu() {
+    this.setState((currentState) => ({ submenuOpened: !currentState.submenuOpened }));
   }
 
   render() {
@@ -36,25 +63,29 @@ export default class ToolboxShapeOption extends Component {
       <div>
         <button
           className="action-btn"
-          onClick={() => {
-            this.addShape(SHAPE_TYPES.ARROW);
-            this.props.optionClickHandler(TOOLBOX_OPTION.ADD_SHAPE);
-          }}
+          onClick={this.toggleSubmenu}
           disabled={!this.props.screenshotImage}
         >
           <svg className="action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M512 320v160c0 17.67-14.33 32-32 32H320c-17.67 0-32-14.33-32-32V320c0-17.67 14.33-32 32-32h160c17.67 0 32 14.33 32 32zm-384-64C57.31 256 0 313.31 0 384s57.31 128 128 128 128-57.31 128-128-57.31-128-128-128zm351.03-32c25.34 0 41.18-26.67 28.51-48L412.51 16c-12.67-21.33-44.35-21.33-57.02 0l-95.03 160c-12.67 21.33 3.17 48 28.51 48h190.06z" />
           </svg>
         </button>
-        {/* <ul>
-              <li>
-                <button className="action-btn" onClick={() => this.props.addShape('arrow')}>
-                  <svg className="action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z" />
-                  </svg>
-                </button>
-              </li>
-            </ul> */}
+        <ul className={!this.state.submenuOpened && 'm-hidden'}>
+          <li>
+            <button className="action-btn" onClick={() => this.addShape(SHAPE_TYPES.ARROW)}>
+              <svg className="action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z" />
+              </svg>
+            </button>
+          </li>
+          <li>
+            <button className="action-btn" onClick={() => this.addShape(SHAPE_TYPES.RECTANGLE)}>
+              <svg className="action-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M512 512H0V0h512v512z" />
+              </svg>
+            </button>
+          </li>
+        </ul>
       </div>
     );
   }
